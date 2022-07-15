6825b7a9fde63641555e7bf9c4579f4410438245
Jovylle <jovyllebermudez@gmail.com>
2022-07-16 02:25:13 +0800

    home and initla header

---
+++ src/theproj/Heeeder.tsx
@@ -0,0 +1,37 @@
+
+
+import reactLogo from '../assets/react.svg'
+
+function Heeder() {
+
+  return (
+    <section className='border mb-8'>
+      <div className="container max-w-[1280px] text-2xl mx-auto">
+        <div className='flex justify-between'>
+          <div className='mx-8 flex items-center'>
+            <a href="/">
+              <img src={reactLogo} className="logo inline-block react" alt="React logo" />
+            </a>
+          </div>
+          <div className='mx-8 flex items-center'>
+            <a href="/" className='mx-4'>
+              Page1
+            </a>
+            <a href="/" className='mx-4'>
+              Page2
+            </a>
+          </div>
+          <div className='mx-8 py-2'>
+            <a href="/" className=''>
+              <button className='px-4 text-base py-2 bg-slate-700 text-white rounded-lg'>
+                Amazing
+              </button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Heeder
